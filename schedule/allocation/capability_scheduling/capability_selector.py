9d782afaa298ff38c16a5173b45847553fcf4892
rejman <dddomino@gmail.com>
2025-07-09 12:52:41 +0200

    Implemented Capability Selector

---
+++ schedule/allocation/capability_scheduling/capability_selector.py
@@ -0,0 +1,37 @@
+from __future__ import annotations
+
+from enum import StrEnum, auto
+
+import attrs as a
+
+from schedule.shared.capability import Capability
+
+
+class SelectingPolicy(StrEnum):
+    ALL_SIMULTANEOUSLY = auto()
+    ONE_OF_ALL = auto()
+
+
+@a.frozen
+class CapabilitySelector:
+    capabilities: set[Capability]
+    selecting_policy: SelectingPolicy
+
+    @staticmethod
+    def can_perform_all_at_the_time(
+        capabilities: set[Capability],
+    ) -> CapabilitySelector:
+        return CapabilitySelector(capabilities, SelectingPolicy.ALL_SIMULTANEOUSLY)
+
+    @staticmethod
+    def can_perform_one_of(capabilities: set[Capability]) -> CapabilitySelector:
+        return CapabilitySelector(capabilities, SelectingPolicy.ONE_OF_ALL)
+
+    def can_perform(self, *capabilities: Capability) -> bool:
+        # TODO: consider extracting algorithms into Policy functions using Strategy pattern  # noqa: FIX002, TD002
+        if len(capabilities) == 1:
+            return capabilities[0] in self.capabilities
+
+        return self.selecting_policy == SelectingPolicy.ALL_SIMULTANEOUSLY and self.capabilities.issuperset(
+            set(capabilities)
+        )
